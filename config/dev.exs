0b32006967595bd24a0c1aa8946b0157da73c992
Chuck Irvine <chuck.irvine@icloud.com>
2023-04-06 19:49:04 -0500

    set log level to info

--- config/dev.exs
+++ config/dev.exs
@@ -67,6 +67,7 @@ config :i_kno, dev_routes: true
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
+config :logger, level: :info
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
