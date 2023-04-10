2a3415a03e040c91eae47afb83e4672996af6b66
Chuck Irvine <chuck.irvine@icloud.com>
2023-04-09 19:36:01 -0500

    moved user files to user dir

--- lib/i_kno/knowledge.ex
+++ lib/i_kno/knowledge.ex
@@ -8,192 +8,59 @@ defmodule IKno.Knowledge do
 
   alias IKno.Knowledge.Topic
 
-  @doc """
-  Returns the list of topics.
-
-  ## Examples
-
-      iex> list_topics()
-      [%Topic{}, ...]
-
-  """
   def list_topics do
     Repo.all(Topic)
   end
+  def list_subject_topics do
+    Repo.all(Topic)
+  end
 
-  @doc """
-  Gets a single topic.
-
-  Raises `Ecto.NoResultsError` if the Topic does not exist.
-
-  ## Examples
-
-      iex> get_topic!(123)
-      %Topic{}
-
-      iex> get_topic!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_topic!(id), do: Repo.get!(Topic, id)
 
-  @doc """
-  Creates a topic.
-
-  ## Examples
-
-      iex> create_topic(%{field: value})
-      {:ok, %Topic{}}
-
-      iex> create_topic(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_topic(attrs \\ %{}) do
     %Topic{}
     |> Topic.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a topic.
-
-  ## Examples
-
-      iex> update_topic(topic, %{field: new_value})
-      {:ok, %Topic{}}
-
-      iex> update_topic(topic, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_topic(%Topic{} = topic, attrs) do
     topic
     |> Topic.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a topic.
-
-  ## Examples
-
-      iex> delete_topic(topic)
-      {:ok, %Topic{}}
-
-      iex> delete_topic(topic)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_topic(%Topic{} = topic) do
     Repo.delete(topic)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking topic changes.
-
-  ## Examples
-
-      iex> change_topic(topic)
-      %Ecto.Changeset{data: %Topic{}}
-
-  """
   def change_topic(%Topic{} = topic, attrs \\ %{}) do
     Topic.changeset(topic, attrs)
   end
 
   alias IKno.Knowledge.Subject
 
-  @doc """
-  Returns the list of subjects.
-
-  ## Examples
-
-      iex> list_subjects()
-      [%Subject{}, ...]
-
-  """
   def list_subjects do
     Repo.all(Subject)
   end
 
-  @doc """
-  Gets a single subject.
-
-  Raises `Ecto.NoResultsError` if the Subject does not exist.
-
-  ## Examples
-
-      iex> get_subject!(123)
-      %Subject{}
-
-      iex> get_subject!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_subject!(id), do: Repo.get!(Subject, id)
 
-  @doc """
-  Creates a subject.
-
-  ## Examples
-
-      iex> create_subject(%{field: value})
-      {:ok, %Subject{}}
-
-      iex> create_subject(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_subject(attrs \\ %{}) do
     %Subject{}
     |> Subject.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a subject.
-
-  ## Examples
-
-      iex> update_subject(subject, %{field: new_value})
-      {:ok, %Subject{}}
-
-      iex> update_subject(subject, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_subject(%Subject{} = subject, attrs) do
     subject
     |> Subject.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a subject.
-
-  ## Examples
-
-      iex> delete_subject(subject)
-      {:ok, %Subject{}}
-
-      iex> delete_subject(subject)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_subject(%Subject{} = subject) do
     Repo.delete(subject)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking subject changes.
-
-  ## Examples
-
-      iex> change_subject(subject)
-      %Ecto.Changeset{data: %Subject{}}
-
-  """
   def change_subject(%Subject{} = subject, attrs \\ %{}) do
     Subject.changeset(subject, attrs)
   end
