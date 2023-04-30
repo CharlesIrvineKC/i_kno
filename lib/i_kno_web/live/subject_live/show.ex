d07e8f5a242ceda09fcc732adf333babdcf86fbe
Chuck Irvine <chuck.irvine@icloud.com>
2023-04-29 20:07:05 -0500

    add learn button

--- lib/i_kno_web/live/subject_live/show.ex
+++ lib/i_kno_web/live/subject_live/show.ex
@@ -29,6 +29,12 @@ defmodule IKnoWeb.SubjectLive.Show do
       type="button"
       class="mt-12 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
     >
+      <a href={~p"/subjects/#{@subject.id}/topics"}>Topics</a>
+    </button>
+    <button
+      type="button"
+      class="mt-12 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+    >
       <a href={~p"/subjects/#{@subject.id}/topics/learn"}>Learn</a>
     </button>
     <button
