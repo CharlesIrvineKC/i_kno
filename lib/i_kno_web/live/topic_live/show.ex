ae01a48212fe3f3982b037f39370b433af62341d
Chuck Irvine <chuck.irvine@icloud.com>
2023-05-02 16:20:11 -0500

    add delete topic button to topic page

--- lib/i_kno_web/live/topic_live/show.ex
+++ lib/i_kno_web/live/topic_live/show.ex
@@ -199,6 +199,11 @@ defmodule IKnoWeb.TopicLive.Show do
     {:noreply, redirect(socket, to: ~p"/subjects/#{topic.subject_id}/topics/#{topic.id}/learn")}
   end
 
+  def handle_event("delete", _, socket) do
+    Knowledge.delete_topic(socket.assigns.topic)
+    {:noreply, redirect(socket, to: ~p"/subjects/#{socket.assigns.topic.subject_id}/topics")}
+  end
+
   defp get_next_topics(socket) do
     case socket.assigns.mode do
       :show ->
@@ -337,6 +342,12 @@ defmodule IKnoWeb.TopicLive.Show do
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Edit
+          </button>
+          <button
+            phx-click="delete"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Delete
           </button>
           <button
             phx-click="new"
