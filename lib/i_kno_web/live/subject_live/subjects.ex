690a4e12e1427e78a49a2e538250bfc5b109a0f1
Chuck Irvine <chuck.irvine@icloud.com>
2023-05-10 08:43:34 -0500

    add title to subjects page

--- lib/i_kno_web/live/subject_live/subjects.ex
+++ lib/i_kno_web/live/subject_live/subjects.ex
@@ -25,7 +25,8 @@ defmodule IKnoWeb.SubjectLive.Subjects do
   def render(assigns) do
     ~H"""
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <h2 class="mb-10 text-4xl font-extrabold dark:text-white">IKno Subjects</h2>
+    <table class="mt-2 w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" class="px-6 py-3">
