0b9cd8a93a08e97052e1545bab5c1ac8fd85f2c6
Chuck Irvine <chuck.irvine@icloud.com>
2023-05-13 08:11:58 -0500

    tweak welcome page to just preview other subjects

--- lib/i_kno_web/live/welcome_live.ex
+++ lib/i_kno_web/live/welcome_live.ex
@@ -10,10 +10,10 @@ defmodule IKnoWeb.WelcomeLive do
     <section class="bg-white dark:bg-gray-900">
       <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          An open door to a universe of knowledge
+          An open door to knowledge
         </h1>
         <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-          At IKno, you will be guided by your curiosity to explore our great treasure, knowledge, the product of the human mind.
+          We have big plans for IKnow. For now, we would like to share what we've learned about Elixir and Phoenix.
         </p>
         <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a
