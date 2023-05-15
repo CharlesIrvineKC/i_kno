aaeab5606c51e3788ff203e5545074bca2f39a40
Chuck Irvine <chuck.irvine@icloud.com>
2023-05-15 09:38:53 -0500

    partial question implementation

--- lib/i_kno_web/live/topic_live/show.ex
+++ lib/i_kno_web/live/topic_live/show.ex
@@ -15,7 +15,7 @@ defmodule IKnoWeb.TopicLive.Show do
     user = Accounts.get_user_by_session_token(user_token)
     is_admin = Accounts.is_admin(subject_id, user.id)
 
-    socket = assign(socket, subject: subject, user: user, is_admin: is_admin)
+    socket = assign(socket, subject: subject, user: user, is_admin: is_admin, take_question: false)
 
     {:ok, socket}
   end
@@ -83,6 +83,16 @@ defmodule IKnoWeb.TopicLive.Show do
       prereqs: prereqs,
       mode: :show
     )
+  end
+
+  def handle_event("post-question", _, socket) do
+    socket = assign(socket, take_question: false)
+    {:noreply, socket}
+  end
+
+  def handle_event("take-question", _, socket) do
+    socket = assign(socket, take_question: !socket.assigns.take_question)
+    {:noreply, socket}
   end
 
   def handle_event("new", _, socket) do
@@ -357,7 +367,37 @@ defmodule IKnoWeb.TopicLive.Show do
             <%= Highlighter.highlight(Earmark.as_html!(@topic.description)) |> Phoenix.HTML.raw() %>
           </section>
         </p>
-        <div class="mt-12">
+
+        <button
+          type="button"
+          phx-click="take-question"
+          class="mt-2 px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <%= if @take_question, do: "Cancel Question", else: "Ask Question" %>
+        </button>
+
+        <form :if={@take_question} phx-submit="post-question" class="mt-5">
+          <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+            <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+              <textarea
+                id="comment"
+                rows="1"
+                class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                placeholder="Ask a question"
+                required
+              />
+            </div>
+            <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+              <button
+                type="submit"
+                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+              >
+                Post your question
+              </button>
+            </div>
+          </div>
+        </form>
+        <div class="mt-8">
           <button
             :if={!@is_known}
             phx-click="understood"
