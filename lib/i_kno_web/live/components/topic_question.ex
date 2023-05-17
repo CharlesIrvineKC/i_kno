7f9627742eeff81457d688b7776922183d0fcde7
Chuck Irvine <chuck.irvine@icloud.com>
2023-05-17 18:02:08 -0500

    reduce height of ask question button

--- lib/i_kno_web/live/components/topic_question.ex
+++ lib/i_kno_web/live/components/topic_question.ex
@@ -48,7 +48,7 @@ defmodule IKnoWeb.Components.TopicQuestion do
           type="button"
           phx-click="take-question"
           phx-target={@myself}
-          class="h-10 mt-2 px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="h-8 mt-2 px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           <%= if @take_question, do: "Cancel Question", else: "Ask Question" %>
         </button>
