00f9a12fbfb654b48647a92242e027e496f8c4f4
Chuck Irvine <chuck.irvine@icloud.com>
2023-05-26 12:30:27 -0500

    minor changes

--- lib/i_kno_web/live/topic_live/show.ex
+++ lib/i_kno_web/live/topic_live/show.ex
@@ -350,7 +350,7 @@ defmodule IKnoWeb.TopicLive.Show do
       href="#"
       class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
     >
-      Reset Subject
+      Review
     </a>
     """
   end
