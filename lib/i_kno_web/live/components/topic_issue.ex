b84fcbbd4d462c15e4953395c57d6058613b8acd
Chuck Irvine <chuck.irvine@icloud.com>
2023-05-29 17:00:47 -0500

    convert issue button to link

--- lib/i_kno_web/live/components/topic_issue.ex
+++ lib/i_kno_web/live/components/topic_issue.ex
@@ -39,16 +39,16 @@ defmodule IKnoWeb.Components.TopicIssue do
     <div>
       <form :if={@take_issue} phx-submit="post-issue" phx-target={@myself} class="mt-5">
         <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-        <div class="border-b border-gray-200 px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
-          <textarea
-            id="summary"
-            name="summary"
-            rows="1"
-            class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-            placeholder="Summarize issue"
-            required
-          />
-        </div>
+          <div class="border-b border-gray-200 px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+            <textarea
+              id="summary"
+              name="summary"
+              rows="1"
+              class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+              placeholder="Summarize issue"
+              required
+            />
+          </div>
           <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
             <textarea
               id="comment"
@@ -70,14 +70,14 @@ defmodule IKnoWeb.Components.TopicIssue do
         </div>
       </form>
       <div>
-        <button
-          type="button"
+        <a
+          href="#"
           phx-click="take-issue"
           phx-target={@myself}
-          class="h-8 mt-2 px-3 py-2 text-xs text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="inline-flex items-center text-sm font-medium text-lime-600 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
         >
           <%= if @take_issue, do: "Cancel Issue", else: "Report Issue" %>
-        </button>
+        </a>
         <div
           :if={@issue_posted}
           id="toast-success"
