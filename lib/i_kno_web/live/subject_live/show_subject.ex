98f33ac11aa03f7a1611949666be82b92ac97065
Chuck Irvine <chuck.irvine@icloud.com>
2023-05-31 15:00:54 -0500

    Add message display

--- lib/i_kno_web/live/subject_live/show_subject.ex
+++ lib/i_kno_web/live/subject_live/show_subject.ex
@@ -20,7 +20,9 @@ defmodule IKnoWeb.SubjectLive.ShowSubject do
         is_admin: is_admin,
         admins: admins,
         is_super_user: is_super_user,
-        edit_admins: edit_admins
+        edit_admins: edit_admins,
+        display_message: false,
+        message: ""
       )
 
     {:ok, socket}
@@ -46,9 +48,15 @@ defmodule IKnoWeb.SubjectLive.ShowSubject do
 
   def handle_event("publish-subject", _, socket) do
     subject = socket.assigns.subject
+    message = if subject.is_published, do: "Subject Un-Puplished", else: "Subject Published"
     Knowledge.update_subject(subject, %{:is_published => !subject.is_published})
     subject = Knowledge.get_subject!(subject.id)
-    socket = assign(socket, subject: subject)
+    socket = assign(socket, subject: subject, display_message: true, message: message)
+    {:noreply, socket}
+  end
+
+  def handle_event("close-message", _, socket) do
+    socket = assign(socket, display_message: false)
     {:noreply, socket}
   end
 
@@ -215,7 +223,7 @@ defmodule IKnoWeb.SubjectLive.ShowSubject do
     <p class="mb-3 text-black dark:text-gray-400">
       <%= @subject.summary %>
     </p>
-    <div class="mb-5 border rounded border-grey-900 p-3">
+    <div class="mb-3 border rounded border-grey-900 p-3">
       <p class="text-black dark:text-gray-400">
         <section class="markdown">
           <%= Earmark.as_html!(@subject.description) |> Phoenix.HTML.raw() %>
@@ -265,8 +273,62 @@ defmodule IKnoWeb.SubjectLive.ShowSubject do
     """
   end
 
+  def render_message(assigns) do
+    ~H"""
+    <div
+      :if={@display_message}
+      id="toast-success"
+      class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+      role="alert"
+    >
+      <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+            clip-rule="evenodd"
+          >
+          </path>
+        </svg>
+        <span class="sr-only">Check icon</span>
+      </div>
+      <div class="ml-3 text-sm font-normal"><%= @message %></div>
+      <button
+        type="button"
+        phx-click="close-message"
+        class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        data-dismiss-target="#toast-success"
+        aria-label="Close"
+      >
+        <span class="sr-only">Close</span>
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          >
+          </path>
+        </svg>
+      </button>
+    </div>
+    """
+  end
+
   def render(assigns) do
     ~H"""
+    <.render_message display_message={@display_message} message={@message}/>
     <div class="h-14">
       <.render_breadcrumb subject={@subject} />
       <.render_searchbox />
