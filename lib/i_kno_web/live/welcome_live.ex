69b6d97c107a4910db506d4c8b0d8209c23d8922
Chuck Irvine <chuck.irvine@icloud.com>
2023-06-05 09:42:24 -0500

    cosmetic

--- lib/i_kno_web/live/welcome_live.ex
+++ lib/i_kno_web/live/welcome_live.ex
@@ -14,14 +14,14 @@ defmodule IKnoWeb.WelcomeLive do
     <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
       Welcome to IKno
     </h1>
-    <p class="mb-3 text-gray-900 dark:text-gray-400">
+    <p class="mb-3 text-xl text-gray-900 dark:text-gray-400">
       <strong>IKno</strong>
-      is an experimental application for authoring and presenting educational subject matter.
+      is an prototype application for authoring and presenting educational subject matter. 
     </p>
-    <p class="mb-3 text-gray-900 dark:text-gray-400">
+    <p class="mb-3 text-xl text-gray-900 dark:text-gray-400">
       If you register and log in, you can pick any topic that you would like to learn and IKno will present information in an optimal order, taking into account what you have already learned.
     </p>
-    <p class="mb-3 text-gray-900 dark:text-gray-400">
+    <p class="mb-3 text-xl text-gray-900 dark:text-gray-400">
       If you would prefer not to register just yet, you can still explore our subjects. Just press the button below.
     </p>
     <button
