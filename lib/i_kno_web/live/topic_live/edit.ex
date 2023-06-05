d25f4cac7a66dd17664da13b9766f10d267bee94
Chuck Irvine <chuck.irvine@icloud.com>
2023-06-05 12:11:53 -0500

    delete task only display

--- lib/i_kno_web/live/topic_live/edit.ex
+++ lib/i_kno_web/live/topic_live/edit.ex
@@ -83,16 +83,6 @@ defmodule IKnoWeb.TopicLive.Edit do
             rows="25"
             class="w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           ><%= @topic.description %></textarea>
-          <label class="relative inline-flex items-center cursor-pointer">
-            <%= if @topic.is_task do %>
-              <input checked type="checkbox" id="is_task" name="is_task" class="sr-only peer" />
-            <% else %>
-              <input type="checkbox" id="is_task" name="is_task" class="sr-only peer" />
-            <% end %>
-            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4    peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
-            </div>
-            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Task?</span>
-          </label>
         </div>
         <button
           type="submit"
