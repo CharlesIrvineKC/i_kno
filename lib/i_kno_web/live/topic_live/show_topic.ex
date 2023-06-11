13d5e3e93da02bafcdfb34b7512434c114921da3
Chuck Irvine <chuck.irvine@icloud.com>
2023-06-11 17:50:42 -0500

    bug fix: call obsolete event handler

--- lib/i_kno_web/live/topic_live/show_topic.ex
+++ lib/i_kno_web/live/topic_live/show_topic.ex
@@ -214,7 +214,6 @@ defmodule IKnoWeb.TopicLive.ShowTopic do
       </button>
       <button
         :if={@is_admin}
-        phx-click="new"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         <a href={"/subjects/#{@topic.subject_id}/topics/new"}>New</a>
