6578cee1d0842f5ac294db4dea133a4709bce2c8
Chuck Irvine <chuck.irvine@icloud.com>
2023-06-13 06:55:56 -0500

    bug fix

--- lib/i_kno_web/live/subject_live/show_subject.ex
+++ lib/i_kno_web/live/subject_live/show_subject.ex
@@ -215,7 +215,7 @@ defmodule IKnoWeb.SubjectLive.ShowSubject do
       phx-click="delete-subject"
       class="mt-12 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none text-white focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
     >
-      <a href={~p"/subjects/#{@subject.id}/edit"}>Delete</a>
+      Delete
     </button>
     <button
       :if={@is_superuser}
