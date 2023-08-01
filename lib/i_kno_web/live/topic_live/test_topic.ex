bec8f17526210252ebd5775ab107ff394fd3bd7c
Chuck Irvine <chuck.irvine@icloud.com>
2023-08-01 13:46:52 -0500

    Implement review topic from test summary page

--- lib/i_kno_web/live/topic_live/test_topic.ex
+++ lib/i_kno_web/live/topic_live/test_topic.ex
@@ -45,6 +45,12 @@ defmodule IKnoWeb.TopicLive.TestTopic do
     end
   end
 
+  def handle_event("review-topic", _, socket) do
+    %{testing_topic: topic, subject: subject, user: user} = socket.assigns
+    Knowledge.reset_learn_topic_progress(topic.id, user.id)
+    {:noreply, redirect(socket, to: ~p"/subjects/#{subject.id}/topics/#{topic.id}/learn")}
+  end
+
   def handle_event("retake-test", _, socket) do
     %{testing_topic: testing_topic, user: user} = socket.assigns
 
@@ -147,6 +153,7 @@ defmodule IKnoWeb.TopicLive.TestTopic do
     </ul>
     <button
       type="button"
+      phx-click="review-topic"
       class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
     >
       Review Topic
