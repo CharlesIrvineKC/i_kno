62d469e5041f27618e6945d5a40f3db0331e5024
Chuck Irvine <chuck.irvine@icloud.com>
2023-08-05 09:09:42 -0500

    added breadcrumb to test subject

--- lib/i_kno_web/live/subject_live/test_subject.ex
+++ lib/i_kno_web/live/subject_live/test_subject.ex
@@ -7,16 +7,17 @@ defmodule IKnoWeb.SubjectLive.TestSubject do
 
   def mount(%{"subject_id" => subject_id}, %{"user_token" => user_token}, socket) do
     subject_id = String.to_integer(subject_id)
+    subject = Knowledge.get_subject!(subject_id)
     user = Accounts.get_user_by_session_token(user_token)
-    question = Knowledge.get_unanswered_question(subject_id, user.id)
+    question = Knowledge.get_unanswered_question(subject.id, user.id)
 
     if question do
       answers = get_answers(question)
 
-      socket = assign(socket, question: question, answers: answers, subject_id: subject_id, user: user)
+      socket = assign(socket, question: question, answers: answers, subject: subject, user: user)
       {:ok, socket}
     else
-      {:ok, redirect(socket, to: ~p"/subjects/#{subject_id}/topics")}
+      {:ok, redirect(socket, to: ~p"/subjects/#{subject.id}/topics")}
     end
   end
 
@@ -39,7 +40,7 @@ defmodule IKnoWeb.SubjectLive.TestSubject do
   end
 
   def handle_event("submit-mc-answers", params, socket) do
-    %{answers: answers, question: question, user: user, subject_id: subject_id} = socket.assigns
+    %{answers: answers, question: question, user: user, subject: subject} = socket.assigns
 
     passed? =
       answers
@@ -53,10 +54,10 @@ defmodule IKnoWeb.SubjectLive.TestSubject do
       question_id: question.id,
       user_id: user.id,
       topic_id: question.topic_id,
-      subject_id: subject_id
+      subject_id: subject.id
     })
 
-    question = Knowledge.get_unanswered_question(subject_id, user.id)
+    question = Knowledge.get_unanswered_question(subject.id, user.id)
 
     if question do
       answers = get_answers(question)
@@ -64,12 +65,12 @@ defmodule IKnoWeb.SubjectLive.TestSubject do
       socket = assign(socket, question: question, answers: answers)
       {:noreply, socket}
     else
-      {:noreply, redirect(socket, to: ~p"/subjects/#{socket.assigns.subject_id}/topics")}
+      {:noreply, redirect(socket, to: ~p"/subjects/#{socket.assigns.subject.id}/topics")}
     end
   end
 
   def handle_event("submit-tf-answer", %{"true?" => true?}, socket) do
-    %{question: question, user: user, subject_id: subject_id} = socket.assigns
+    %{question: question, user: user, subject: subject} = socket.assigns
     true? = String.to_atom(true?)
     status = if true? == question.is_correct, do: :passed, else: :failed
 
@@ -78,10 +79,10 @@ defmodule IKnoWeb.SubjectLive.TestSubject do
       question_id: question.id,
       user_id: user.id,
       topic_id: question.topic_id,
-      subject_id: subject_id
+      subject_id: subject.id
     })
 
-    question = Knowledge.get_unanswered_question(subject_id, user.id)
+    question = Knowledge.get_unanswered_question(subject.id, user.id)
 
     if question do
       answers = get_answers(question)
@@ -89,18 +90,121 @@ defmodule IKnoWeb.SubjectLive.TestSubject do
       socket = assign(socket, question: question, answers: answers)
       {:noreply, socket}
     else
-      {:noreply, redirect(socket, to: ~p"/subjects/#{socket.assigns.subject_id}/topics")}
+      {:noreply, redirect(socket, to: ~p"/subjects/#{subject.id}/topics")}
     end
+  end
+
+  def render_breadcrumb(assigns) do
+    topic = Knowledge.get_topic!(assigns.topic_id)
+    assigns = Map.put(assigns, :topic, topic)
+    ~H"""
+    <div class="h-14">
+      <nav class="pt-3 inline-block " aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+          <li class="inline-flex items-center">
+            <a
+              href={~p"/subjects"}
+              class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+            >
+              <svg
+                aria-hidden="true"
+                class="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+                </path>
+              </svg>
+              Subjects
+            </a>
+          </li>
+          <li>
+            <div class="flex items-center">
+              <svg
+                aria-hidden="true"
+                class="w-6 h-6 text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                >
+                </path>
+              </svg>
+              <a
+                href={~p"/subjects/#{@subject.id}/topics"}
+                class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+              >
+                <%= @subject.name %>
+              </a>
+            </div>
+          </li>
+
+          <li :if={@topic} aria-current="page">
+            <div class="flex items-center">
+              <svg
+                aria-hidden="true"
+                class="w-6 h-6 text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                >
+                </path>
+              </svg>
+              <a
+                href={~p"/subjects/#{@subject.id}/topics/#{@topic.id}"}
+                class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400"
+              >
+                <%= @topic.name %>
+              </a>
+            </div>
+          </li>
+        </ol>
+      </nav>
+      <button
+        type="submit"
+        phx-click="search"
+        class="float-right p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <svg
+          class="w-5 h-5"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          >
+          </path>
+        </svg>
+        <span class="sr-only">Search</span>
+      </button>
+    </div>
+    """
   end
 
   def render(assigns) do
     ~H"""
-      <.live_component
-        module={AnswerQuestion}
-        id="test-subject-answer-question"
-        question={@question}
-        answers={@answers}
-      />
+    <.render_breadcrumb subject={@subject} topic_id={@question.topic_id} />
+    <.live_component
+      module={AnswerQuestion}
+      id="test-subject-answer-question"
+      question={@question}
+      answers={@answers}
+    />
     """
   end
 end
