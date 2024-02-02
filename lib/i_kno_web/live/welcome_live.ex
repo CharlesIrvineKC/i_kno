1d83b57932f4745fc6cd975e52cd190677abf274
Chuck Irvine <chuck.irvine@icloud.com>
2024-02-02 10:09:19 -0600

    update welcome page with author info

--- lib/i_kno_web/live/welcome_live.ex
+++ lib/i_kno_web/live/welcome_live.ex
@@ -44,7 +44,9 @@ defmodule IKnoWeb.WelcomeLive do
           Or Register Later
         </h5>
         <p class="font-normal text-gray-700 dark:text-gray-400">
-          If you would prefer not to register just yet, you can still explore our subjects. Just press the <b>List of Subjects</b> button below.
+          If you would prefer not to register just yet, you can still explore our subjects. Just press the
+          <b>List of Subjects</b>
+          button below.
         </p>
         <p class="mt-2 font-normal text-gray-700 dark:text-gray-400">
           However, you definitely won't <b>get it</b> until you register and sign in.
@@ -58,14 +60,18 @@ defmodule IKnoWeb.WelcomeLive do
           Subjects and Topics
         </h5>
         <p class="font-normal text-gray-700 dark:text-gray-400">
-          Information in IKno is organized into <b>subjects</b> and <b>topics</b>. A <b>subject</b> is composed of a set of <b>topics</b>.
+          Information in IKno is organized into <b>subjects</b>
+          and <b>topics</b>. A <b>subject</b>
+          is composed of a set of <b>topics</b>.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
           IKno knows how topics depend on each other, allowing topics to presented in a sequence that
           optimizes learning effectiveness.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
-          Currently, IKno subjects are incomplete and have been authored solely for the purpose of testing IKno. The most complete subject and one that will be expanded upon is <b>Business Process Management</b>. There is also a subject describing <b>IKno</b> itself that you may find of interest.
+          Currently, IKno subjects are incomplete and have been authored solely for the purpose of testing IKno. The most complete subject and one that will be expanded upon is <b>Business Process Management</b>. There is also a subject describing
+          <b>IKno</b>
+          itself that you may find of interest.
         </p>
       </a>
       <a
@@ -76,21 +82,44 @@ defmodule IKnoWeb.WelcomeLive do
           Learning Mode
         </h5>
         <p class="font-normal text-gray-700 dark:text-gray-400">
-          <b>Learning mode</b> refers to having IKno step you through topics based on what you already know. Topics are ordered from the most fundamental to the most complex.
+          <b>Learning mode</b>
+          refers to having IKno step you through topics based on what you already know. Topics are ordered from the most fundamental to the most complex.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
-          There are two ways to use learning mode: at the <b>subject level</b> or at the <b>topic level</b>.
+          There are two ways to use learning mode: at the <b>subject level</b>
+          or at the <b>topic level</b>.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
           At the <b>subject</b> level, IKno will present topics from the subject as a whole.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
-          At the <b>topic</b> level, IKno will present only those topics required for the understanding of the selected topic.
+          At the <b>topic</b>
+          level, IKno will present only those topics required for the understanding of the selected topic.
         </p>
       </a>
       <a
         href="#"
-        class="mb-5 mr-5 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        class="mb-5 mr-5 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg
+        shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Authoring Mode
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+          <b>Authoring mode</b>
+          is used by content authors to create and update subject content.
+        </p>
+        <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
+          One or more users can be configured as authors for a given subject. Subject authors own their content and can export it in MarkDown format at any time.
+        </p>
+        <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
+          At some point, IKno will implement mechanisms for revenue generation and revenue sharing with authors. Early adopters will be granted especially generous terms.
+        </p>
+      </a>
+      <a
+        href="#"
+        class="mb-5 mr-5 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg
+        shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       >
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Testing Mode
@@ -99,16 +128,19 @@ defmodule IKnoWeb.WelcomeLive do
           In <b>Testing mode</b>, IKno will present questions on the topics that you have learned.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
-          Again, there are two ways to use testing mode: at the <b>subject level</b> or at the <b>topic level</b>.
+          Again, there are two ways to use testing mode: at the <b>subject level</b>
+          or at the <b>topic level</b>.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
           At the <b>subject</b> level, IKno will present questions from the subject as a whole.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
-          At the <b>topic</b> level, IKno will present only those questions required relevant to a selected topic.
+          At the <b>topic</b>
+          level, IKno will present only those questions required relevant to a selected topic.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
-          <b>Note:</b> If the author of an IKno subject hasn't written any questions, testing mode won't be available or visible.
+          <b>Note:</b>
+          If the author of an IKno subject hasn't written any questions, testing mode won't be available or visible.
         </p>
       </a>
       <a
@@ -122,7 +154,9 @@ defmodule IKnoWeb.WelcomeLive do
           Every effort has been made to make IKno as <b>simple as possible</b>, so you will likely be able to surmise, for the most part, how things work.
         </p>
         <p class="mt-3 font-normal text-gray-700 dark:text-gray-400">
-          However, if you want a better understanding or are just curious, you can find detailed information describing IKno by clicking on the <b>Explore</b> button below and then clicking in the IKno subject.
+          However, if you want a better understanding or are just curious, you can find detailed information describing IKno by clicking on the
+          <b>Explore</b>
+          button below and then clicking in the IKno subject.
         </p>
       </a>
     </div>
