083ee984612a86fbaf243ae91f2a64962bff54b7
Chuck Irvine <chuck.irvine@icloud.com>
2024-02-04 19:24:27 -0600

    cosmetic tweaks

--- lib/i_kno_web/live/welcome_live.ex
+++ lib/i_kno_web/live/welcome_live.ex
@@ -163,7 +163,7 @@ defmodule IKnoWeb.WelcomeLive do
     <button
       phx-click="list-subjects"
       type="button"
-      class="mb-14 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
+      class="mb-20 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
     >
       Explore Subjects
     </button>
